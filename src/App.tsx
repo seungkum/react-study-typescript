ddef4d9e2d834e8dbfee95db2245b1575468283e
baek_seungmin <min7490@naver.com>
2023-04-10 20:47:06 +0900

    markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <h1>React study</h1>
+            </header>
+            <hr />
+            <div className='container'>
+                <nav className='nav'>
+                    <ul>
+                        <li>
+                            <h2>Users</h2>
+                        </li>
+                        <li>
+                            <h2>Search</h2>
+                        </li>
+                    </ul>
+                </nav>
+                <hr />
+                <section className='contents'>
+                    <div>
+                        <h3>Users</h3>
+                        <p>Contents</p>
+                    </div>
+                </section>
+                <hr />
+            </div>
+            <footer>Copyright</footer>
+        </div>
+    );
 }
 
 export default App;
